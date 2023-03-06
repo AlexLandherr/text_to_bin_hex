ff465c2413d24c55f4514c27a5c5b7e84739f33f
AlexLandherr <alex.landherr@gmail.com>
2023-03-06 15:35:57 +0000

    Added text input from user & revised printout of converted data.

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,32 @@
 #include "include/counters.h"
 #include <iostream>
 #include <string>
+#include <limits>
 
 int main() {
-    std::string text = "Alex Landherr";
+    std::string text;
+    while (true) {
+        std::cout << "Text to convert: ";
+        std::getline(std::cin, text);
+        if (std::cin) {
+            break;
+        } else if (!std::cin) {
+            std::cin.clear(); //Resets error state for std::cin.
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //Flushes std::cin buffer and removes bad input.
+            std::cerr << "Byte value was out of range!" << '\n';
+        }
+    }
+
+    std::cout << "    ****    " << '\n';
     std::cout << "Original text is: " << text << '\n';
-    std::cout << "text bit count: " << counters::bit_count(text) << '\n';
-    std::cout << "text byte count: " << counters::byte_count(text) << '\n';
-    std::cout << "text to binary string: " << conv::to_binary_str(text) << '\n';
-    std::cout << "text to hexadecimal string: " << conv::to_hexadecimal_str(text) << '\n';
-    std::cout << "text hex string length: " << counters::hex_count(text) << '\n';
+    std::cout << " **** Binary & Hexadecimal **** " << '\n';
+    std::cout << "Binary string: " << conv::to_binary_str(text) << '\n';
+    std::cout << "Hexadecimal string: " << conv::to_hexadecimal_str(text) << '\n';
+    std::cout << '\n';
+    std::cout << " **** Lengths **** " << '\n';
+    std::cout << "Bit count: " << counters::bit_count(text) << '\n';
+    std::cout << "Byte count: " << counters::byte_count(text) << '\n';
+    std::cout << "Hex string length: " << counters::hex_count(text) << '\n';
 
     return 0;
 }
